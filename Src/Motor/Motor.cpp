719f986d8857ad9acca06344e0e0c7f025edd3c1
elisatodd <81906237+elisatodd@users.noreply.github.com>
2023-02-08 00:30:08 +0100

    Arreglo de clang-format

--- Src/Motor/Motor.cpp
+++ Src/Motor/Motor.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <fstream>
 #include "Biblioteca/Biblioteca.h"
 #include <OgreRoot.h>
+#include <fstream>
+#include <iostream>
 
-int main(){
-    Ogre::Root* root;
-    root=new Ogre::Root();
+int main() {
+    Ogre::Root *root;
+    root = new Ogre::Root();
     saluda("XD");
-   /* std::fstream f("Assets\\mapa.txt");
-    if (f.is_open()) {
-        std::cout << "FICHERO ABIERTO\n";
-        f.close();
-    }
-    else {
-        std::cerr << "FICHERO NO ABIERTO";
-        return 1;
-    }*/
-   
-   return 0;
+    /* std::fstream f("Assets\\mapa.txt");
+     if (f.is_open()) {
+         std::cout << "FICHERO ABIERTO\n";
+         f.close();
+     }
+     else {
+         std::cerr << "FICHERO NO ABIERTO";
+         return 1;
+     }*/
+
+    return 0;
 }
