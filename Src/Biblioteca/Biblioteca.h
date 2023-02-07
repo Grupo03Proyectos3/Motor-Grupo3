2414e8c441c3bb2dc2341a9bb688dc27467a3a5d
Javier <javcomas@ucm.es>
2023-02-07 12:53:06 +0100

    Ogre

---
+++ Src/Biblioteca/Biblioteca.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef __BIBLIOTECA_BIBLIOTECA_H
+#define __BIBLIOTECA_BIBLIOTECA_H
+void saluda(const char* name);
+#endif
---
+++ Src/Motor/Motor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include "Biblioteca/Biblioteca.h"
+#include <OgreRoot.h>
+
+int main(){
+    Ogre::Root* root;
+    root=new Ogre::Root();
+    saluda("XD");
+   /* std::fstream f("Assets\\mapa.txt");
+    if (f.is_open()) {
+        std::cout << "FICHERO ABIERTO\n";
+        f.close();
+    }
+    else {
+        std::cerr << "FICHERO NO ABIERTO";
+        return 1;
+    }*/
+   
+   return 0;
+}
