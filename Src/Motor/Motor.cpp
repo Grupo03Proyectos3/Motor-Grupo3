1cfb3adefa6ee5c700eeb21a226237d0cd41600e
anonymous <anonymous@anonymous>
2023-02-08 14:42:52 +0100

    Añadido .editorconfig
    Cumple la misma función que clang-format, pero este archivo se generó automáticamente según los ajustes seleccionados en visual studio

--- Src/Motor/Motor.cpp
+++ Src/Motor/Motor.cpp
@@ -3,19 +3,20 @@
 #include <fstream>
 #include <iostream>
 
-int main() {
-    Ogre::Root *root;
-    root = new Ogre::Root();
-    saluda("XD");
-    /* std::fstream f("Assets\\mapa.txt");
-     if (f.is_open()) {
-         std::cout << "FICHERO ABIERTO\n";
-         f.close();
-     }
-     else {
-         std::cerr << "FICHERO NO ABIERTO";
-         return 1;
-     }*/
+int main()
+{
+	Ogre::Root* root;
+	root = new Ogre::Root();
+	saluda("XD");
+/* std::fstream f("Assets\\mapa.txt");
+ if (f.is_open()) {
+	 std::cout << "FICHERO ABIERTO\n";
+	 f.close();
+ }
+ else {
+	 std::cerr << "FICHERO NO ABIERTO";
+	 return 1;
+ }*/
 
-    return 0;
+	return 0;
 }
