48f0c9a3d87d64154b3382c8139026d8bd47adbb
Javier <javcomas@ucm.es>
2023-02-10 10:19:50 +0100

    PRUEBA1

--- Src/Motor/Motor.cpp
+++ Src/Motor/Motor.cpp
@@ -8,15 +8,15 @@ int main()
 	Ogre::Root* root;
 	root = new Ogre::Root();
 	saluda("XD");
-/* std::fstream f("Assets\\mapa.txt");
- if (f.is_open()) {
-	 std::cout << "FICHERO ABIERTO\n";
-	 f.close();
- }
- else {
-	 std::cerr << "FICHERO NO ABIERTO";
-	 return 1;
- }*/
+	/* std::fstream f("Assets\\mapa.txt");
+	if (f.is_open()) {
+		std::cout << "FICHERO ABIERTO\n";
+		f.close();
+	}
+	else {
+		std::cerr << "FICHERO NO ABIERTO";
+		return 1;
+	}*/
 
 	return 0;
 }
