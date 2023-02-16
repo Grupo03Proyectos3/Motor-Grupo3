bdaa3d7158e23e1aa11c75ece7bfccb651c467f5
ssimoanto <simoanto@ucm.es>
2023-02-16 15:48:58 +0100

    Revert "Entity base añadida"
    This reverts commit ee0c4acbeae9c81b423d4c2c1c69c414cabc86c7.

--- Projects/ECS/Component.h
+++ Projects/ECS/Component.h
@@ -1,49 +1,12 @@
 #pragma once
 
 #ifndef __COMPONENT_H__
-#define __COMPONENT_H__
+#    define __COMPONENT_H__
 
 namespace ecs
 {
     struct Component
     {
-      public:
-        Component();
-
-        // Destroys the component.
-        // Careful! ent_ and mngr_ should not be destroyed
-        //
-        virtual ~Component();
-        // This method is used to set the context, which is a
-        // reference to the entity in which the component is
-        // installed and a reference to the manager. It will
-        // be called by Entity when adding a component.
-        //
-        inline void setContext(/*Entity* ent, Manager* mngr*/);
-
-        // We assume that initComponent will be called when adding a
-        // component to an entity, immediately after setContext.
-        //
-        virtual void initComponent();
-        // A component has an update method, and by default it
-        // does nothing -- some components will not override this
-        // method.
-        //
-        virtual void update();
-
-        // A component has a render method, and by default it
-        // does nothing -- some components will not override this
-        // method.
-        //
-        virtual void render();
-
-      protected:
-        // as mentions above, when using systems these fields are not
-        // really needed, but we keep them for now from the same reason that
-        // we keep update/render
-        
-        //Entity* ent_;   // a reference to the entity, should not be deleted on destruction
-        //Manager* mngr_; //  a pointer to the manager, should not be deleted on destruction
     };
 } // namespace ecs
 
