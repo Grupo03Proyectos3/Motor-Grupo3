ad4ef10c24a816cd6c8fb6ff5c7b00965b518e77
ssimoanto <simoanto@ucm.es>
2023-02-17 10:28:38 +0100

    message_defs

---
+++ Src/ECS/messages_defs.h
@@ -0,0 +1,30 @@
+// This file is part of the course TPV2@UCM - Samir Genaim
+#pragma once
+
+#ifndef __MESSAGES_DEFS_H__
+#define __MESSAGES_DEFS_H__
+
+#include <cstdint>
+
+#include "ecs.h"
+
+using msgId_type = uint8_t;
+enum msgId : msgId_type
+{
+    MSG_EXAMPLE
+};
+
+struct Message
+{
+    msgId_type id;
+
+    union
+    {
+        struct
+        {
+            ecs::Entity* e;
+        } example_msg;
+    };
+};
+
+#endif
