b1e99c408a1856122e2d82c3903dba7455b33cf5
admont02 <admont02@ucm.es>
2023-02-17 09:33:06 +0100

    ecs defs

---
+++ Src/ECS/ecs_defs.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef __ECS_DEFS_H__
+#define __ECS_DEFS_H__
+
+
+// Lista de componentes
+//
+#define COMPS_LIST       \
+   COMP_EXAMPLE
+
+// Lista de grupos
+//
+#define GROUPS_LIST \
+    GROUP_EXAMPLE
+
+// Lista de Handlers
+//
+#define _HANDLERS_LIST_ \
+    HANDLER_EXAMPLE
+
+// Lista de sistemas
+//
+#define _SYSTEMS_LIST_     \
+    SYSTEM_EXAMPLE
+
+#endif // !__ECS_DEFS__
