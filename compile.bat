2ea79bdd24cca06dea4480362fb00af2aac32850
Miguel González Pérez <miggon23@ucm.es>
2023-02-20 15:06:04 +0100

    Compilación automática de Ogre con compile.bat

---
+++ compile.bat
@@ -0,0 +1,9 @@
+cd Dependencias
+cd Ogre
+
+mkdir Build
+cd Build
+
+cmake -G "Visual Studio 17 2022" -A x64 -S ../Src -DBUILD_COMPONENT_BITES=OFF -DOGRE_BUILD_RENDERSYSTEM_D3D9=OFF
+msbuild  Ogre.sln /p:Configuration=Debug
+msbuild  Ogre.sln /p:Configuration=Release
