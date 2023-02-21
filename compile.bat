b33779396ea79f31c9cf0baa9625238e0a9e3d1b
Miguel González Pérez <miggon23@ucm.es>
2023-02-21 17:48:20 +0100

    .bat Ya no incluye OgreBites

--- compile.bat
+++ compile.bat
@@ -4,6 +4,6 @@ cd Ogre
 mkdir Build
 cd Build
 
-cmake -G "Visual Studio 17 2022" -A x64 -S ../Src -DBUILD_COMPONENT_BITES=OFF -DOGRE_BUILD_RENDERSYSTEM_D3D9=OFF
+cmake -G "Visual Studio 17 2022" -A x64 -S ../Src -DOGRE_BUILD_COMPONENT_BITES=OFF -DOGRE_BUILD_RENDERSYSTEM_D3D9=OFF
 msbuild  Ogre.sln /p:Configuration=Debug
 msbuild  Ogre.sln /p:Configuration=Release
