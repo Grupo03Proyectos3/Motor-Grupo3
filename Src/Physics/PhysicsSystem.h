fafd36caabbf47caba9e319fbe8e2b58867ad964
elisatodd <elitodd@ucm.es>
2023-02-27 23:41:41 +0100

    Variables y métodos de PhysicSystem
    Declaración de variables de la clase PhysicSystem
    
    Co-Authored-By: Miguel González Pérez <82326243+miggon23@users.noreply.github.com>

--- Src/Physics/PhysicsSystem.h
+++ Src/Physics/PhysicsSystem.h
@@ -4,7 +4,16 @@
 
 #include "../ECS/System.h"
 
-//class btRigidBody;
+class btRigidBody;
+class btDiscreteDynamicsWorld;
+class btDispatcher;
+class btConstraintSolver;
+class btCollisionConfiguration;
+class btBroadphaseInterface;
+
+class btTransform;
+class btCollisionShape;
+class btGhostObject;
 
 class PhysicsSystem : public ecs::System
 {
@@ -18,7 +27,16 @@ class PhysicsSystem : public ecs::System
     void initSystem() override;
     void update(float t_delta_time) override;
 
+    void addRigidBody(btRigidBody* t_rb);
+    btRigidBody* createRigidBody(btTransform* t_transform, btCollisionShape* t_shape, const float& t_mass);
+    
   private:
+    btDiscreteDynamicsWorld* m_world = nullptr;
+    btDispatcher* m_dispatcher = nullptr;
+    btConstraintSolver* m_solver = nullptr;
+    btCollisionConfiguration* m_collision_config = nullptr;
+    // BroadphaseInterface sirve para identificar pares de objetos que podrían colisionar entre sí
+    btBroadphaseInterface* m_broadphase = nullptr;
 };
 
 #endif
