f9b1d905dac34c880c1f4211bde6018f58d30d7b
admont02 <admont02@ucm.es>
2023-03-21 10:56:45 +0100

    Update ParticleSystem.cpp

--- Src/Render/ParticleSystem.cpp
+++ Src/Render/ParticleSystem.cpp
@@ -26,6 +26,22 @@ void Flamingo::ParticleSystem::initComponent()
     m_particle_system->setEmitting(true);
     m_node->attachObject(m_particle_system);
     m_node->setPosition({0, 60, 0});
+
+    //// Obtener el material de partículas utilizado por el sistema
+    //Ogre::MaterialPtr material = m_particle_system->getMaterial();
+
+    //// Obtener el estado del color del material de partículas
+    //Ogre::Pass* pass = material->getTechnique(0)->getPass(0);
+    //Ogre::ColourValue color = pass->getDiffuse();
+
+    //// Modificar el color del material de partículas
+    //color.r = 1.0f; // Red
+    //color.g = 0.0f; // Green
+    //color.b = 0.0f; // Blue
+    //pass->setDiffuse(color);
+
+    //// Actualizar el material de partículas utilizado por el sistema
+    //m_particle_system->setMaterial(material->getName());
 }
 
 void Flamingo::ParticleSystem::emit()
