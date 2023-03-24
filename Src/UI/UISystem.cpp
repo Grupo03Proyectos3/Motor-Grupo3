8cf7597209375413f926ef0a53034dc0a6d7896d
Javier <javcomas@ucm.es>
2023-03-24 10:37:33 +0100

    props
    Co-Authored-By: sarais02 <82498555+sarais02@users.noreply.github.com>

--- Src/UI/UISystem.cpp
+++ Src/UI/UISystem.cpp
@@ -1,9 +1,11 @@
 #include "UISystem.h"
-
 #include <CEGUI/Base.h>
 #include <CEGUI/Window.h>
 #include <CEGUI/RendererModules/Ogre/Renderer.h>
 #include <iostream>
+
+
+
 
 namespace Flamingo{
     UISystem::UISystem(){
@@ -16,6 +18,11 @@ namespace Flamingo{
     void UISystem::recieve(const Message&){
     }
     void UISystem::initSystem(){
+        //auto sys = m_mngr->getSystem<RenderSystem>();
+        
+        //CEGUI::OgreRenderer& auxiliar = CEGUI::OgreRenderer::bootstrapSystem(*renderWindow);
+        //renderer = &auxiliar;
+
     }
     void UISystem::update(float t_delta_time){
     }
