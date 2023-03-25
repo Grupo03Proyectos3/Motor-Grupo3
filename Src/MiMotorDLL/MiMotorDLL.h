df64ad968dae5f4cca70103a05e8c7b7c5797299
Adrián Montero <82326212+admont02@users.noreply.github.com>
2023-03-25 12:56:47 +0100

    no hay precompilados

---
+++ Src/MiMotorDLL/MiMotorDLL.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef __MY_FLAMINGO__
+#ifdef __MY_FLAMINGO_EXPORT__
+#define __MY_FLAMINGO__ __declspec(dllexport)
+#else
+#define __MY_FLAMINGO__ __declspec(dllimport)
+#endif
+#endif
