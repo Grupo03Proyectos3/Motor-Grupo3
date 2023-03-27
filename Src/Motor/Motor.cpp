e12d2fceb87cb351329b479f8316d5dbf68bd12f
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2023-03-27 19:27:02 +0200

    Update Motor.cpp
    Co-Authored-By: RaulSaavedraRiera <82366515+RaulSaavedraRiera@users.noreply.github.com>

--- Src/Motor/Motor.cpp
+++ Src/Motor/Motor.cpp
@@ -6,7 +6,6 @@
 #include "ECS/InputHandler.h"
 #include "ECS/Manager.h"
 
-
 // PHYSICS
 #include "Physics/PhysicsSystem.h"
 #include "Physics/PlayerController.h"
@@ -16,10 +15,10 @@
 #include "Render/Animator.h"
 #include "Render/Light.h"
 #include "Render/MeshRenderer.h"
-#include "Render/RenderSystem.h"
 #include "Render/ParticleSystem.h"
+#include "Render/RenderSystem.h"
 
-//LUA
+// LUA
 #include "Lua/LuaSystem.h"
 
 // BASE
@@ -31,29 +30,28 @@
 
 // EXTERNAL
 
-
 // C++
 #include <iostream>
 #include <string>
 
 // Carga de mapas
 #include "FlamingoBase/MapReader.h"
-#include <UI/UISystem.h>
-#include <CEGUI/CEGUI.h>
 #include "ResourcesLoader/Loader.h"
+#include <CEGUI/CEGUI.h>
+#include <UI/UISystem.h>
 
 void loadScene(RenderSystem* t_render_sys)
 {
-    MapReader* map_reader = new MapReader();
+    MapReader* map_reader = new MapReader(t_render_sys);
 
-    map_reader->readMap("Assets/Maps/mapPrueba.json", t_render_sys);
+    map_reader->readMap("Assets/Maps/mapPrueba.json");
 
     delete map_reader; // TO DO
 }
 
 int main(int argc, char* argv[])
 {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);  
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     Loader l;
     l.loadDirectories();
@@ -67,20 +65,20 @@ int main(int argc, char* argv[])
 
     RenderSystem* render_sys = m_mngr->addSystem<RenderSystem>(s);
     PhysicsSystem* physics_sys = m_mngr->addSystem<PhysicsSystem>();
-   
-    //Flamingo::UISystem* uiSystem = m_mngr->addSystem<Flamingo::UISystem>();
-   /* CEGUI::DefaultResourceProvider* rp = static_cast<CEGUI::DefaultResourceProvider*>(CEGUI::System::getSingleton().getResourceProvider());
-    rp->setResourceGroupDirectory("Imagesets", "./UI/imagesets/");
-    rp->setResourceGroupDirectory("Fonts", "./UI/fonts/");
-    rp->setResourceGroupDirectory("Schemes", "./UI/schemes/");
-    rp->setResourceGroupDirectory("LookNFeel", "UI/looknfeel/");
-    rp->setResourceGroupDirectory("Layouts", "UI/layouts/");
-    uiSystem->initUIResources();*/
-    
-   // LuaSystem* lua_system = m_mngr->addSystem<LuaSystem>();
+
+    // Flamingo::UISystem* uiSystem = m_mngr->addSystem<Flamingo::UISystem>();
+    /* CEGUI::DefaultResourceProvider* rp = static_cast<CEGUI::DefaultResourceProvider*>(CEGUI::System::getSingleton().getResourceProvider());
+     rp->setResourceGroupDirectory("Imagesets", "./UI/imagesets/");
+     rp->setResourceGroupDirectory("Fonts", "./UI/fonts/");
+     rp->setResourceGroupDirectory("Schemes", "./UI/schemes/");
+     rp->setResourceGroupDirectory("LookNFeel", "UI/looknfeel/");
+     rp->setResourceGroupDirectory("Layouts", "UI/layouts/");
+     uiSystem->initUIResources();*/
+
+    // LuaSystem* lua_system = m_mngr->addSystem<LuaSystem>();
     auto& ihldr = ih();
 
-    Flamingo::Timer* player_timer = new Flamingo::Timer(); 
+    Flamingo::Timer* player_timer = new Flamingo::Timer();
     auto time = player_timer->getElapsedTime();
     auto dt = player_timer->getElapsedTime() - time;
 
@@ -123,15 +121,16 @@ int main(int argc, char* argv[])
     //  m_mngr->setHandler(ecs::HANDLER_EXAMPLE, go);
     // render_sys->getSceneManager()->getSceneActive()->addObjects(ground);
 
-    auto nodo = render_sys->getSceneManager()->getSceneActive()->getSceneRoot();
+   /* auto nodo = render_sys->getSceneManager()->getSceneActive()->getSceneRoot();
     ecs::GameObject* cube_go = m_mngr->addGameObject({ecs::GROUP_RENDER});
     auto cmp2 = ecs::AddComponent<MeshRenderer>(cube_go);
     cmp2->initValues(nodo, render_sys->getSceneManager()->getSceneActive()->getSceneManger(), "cube.mesh", "CubeEntity");
     cmp2->changeMaterial("Prueba/MichaelScott");
     Transform* cmp_tr2 = ecs::AddComponent<Transform>(cube_go);
     cmp_tr2->setPosition(SVector3(0, 0, 0));
-    render_sys->getSceneManager()->getSceneActive()->addObjects(cube_go);
+    render_sys->getSceneManager()->getSceneActive()->addObjects(cube_go);*/
 
+    auto nodo = render_sys->getSceneManager()->getSceneActive()->getSceneRoot();
     ecs::GameObject* light_go = m_mngr->addGameObject({ecs::GROUP_RENDER});
     Light* cmp_light = ecs::AddComponent<Light>(light_go);
     cmp_light->initValues(render_sys->getSceneManager()->getSceneActive()->getSceneManger(), nodo->createChildSceneNode(), "myLight");
@@ -156,14 +155,19 @@ int main(int argc, char* argv[])
     render_sys->getSceneManager()->getSceneActive()->addObjects(cam_go);
 
     Ogre::ParticleSystem* pSys = render_sys->getSceneManager()->getSceneActive()->getSceneManger()->createParticleSystem("psBomba", "PsPrueba/Smoke");
-
     pSys->setEmitting(true);
     Ogre::SceneNode* prueba = render_sys->getSceneManager()->getSceneActive()->getSceneRoot()->createChildSceneNode();
-    prueba->setPosition({0, 60, 0});
+    prueba->setPosition({200, 60, 0});
     prueba->attachObject(pSys);
 
- //   Flamingo::ParticleSystem* pSys = ecs::AddComponent<Flamingo::ParticleSystem>(sinbad_go, scene_active->getSceneManger(), scene_active->getSceneRoot()->createChildSceneNode());
-  
+    Ogre::ParticleSystem* pSys2 = render_sys->getSceneManager()->getSceneActive()->getSceneManger()->createParticleSystem("psBomba2", "PsPrueba/Smoke");
+    pSys2->setEmitting(true);
+    Ogre::SceneNode* prueba2 = render_sys->getSceneManager()->getSceneActive()->getSceneRoot()->createChildSceneNode();
+    prueba2->setPosition({-200, 60, 0});
+    prueba2->attachObject(pSys2);
+
+    //   Flamingo::ParticleSystem* pSys = ecs::AddComponent<Flamingo::ParticleSystem>(sinbad_go, scene_active->getSceneManger(), scene_active->getSceneRoot()->createChildSceneNode());
+
     while (game_playing && !render_sys->getWindow()->isWindowClosed())
     {
         // Delta time en milisegundos
@@ -178,7 +182,7 @@ int main(int argc, char* argv[])
 
         // m_controller->handleInput();
         render_sys->manipulateCamera();
-        ihldr.refresh(); 
+        ihldr.refresh();
         /*
         if (ihldr.keyDownEvent())
         {
@@ -204,119 +208,119 @@ int main(int argc, char* argv[])
 //#include "FlamingoBase/MapReader.h"
 ////#include <Windows.h>
 //
-//using namespace Flamingo;
+// using namespace Flamingo;
 //
 //// Convierte la ruta obtenida al formato de resources.cfg
-//std::string parsePath(std::string t_path)
+// std::string parsePath(std::string t_path)
 //{
-//    std::string new_Path = t_path;          // Creo otro string del mimo tama�o que "path"
-//    for (int i = 0; i < t_path.size(); i++) // En "newPath" me guardo la ruta pero con el formato adecuado
-//    {
-//        if (t_path[i] == '\\')
-//        {
-//            new_Path[i] = '/';
-//        }
-//        else
-//            new_Path[i] = t_path[i];
-//    }
-//    return new_Path;
-//}
+//     std::string new_Path = t_path;          // Creo otro string del mimo tama�o que "path"
+//     for (int i = 0; i < t_path.size(); i++) // En "newPath" me guardo la ruta pero con el formato adecuado
+//     {
+//         if (t_path[i] == '\\')
+//         {
+//             new_Path[i] = '/';
+//         }
+//         else
+//             new_Path[i] = t_path[i];
+//     }
+//     return new_Path;
+// }
 //
-//void findDir(std::filesystem::directory_iterator t_dir, std::ofstream& t_output)
+// void findDir(std::filesystem::directory_iterator t_dir, std::ofstream& t_output)
 //{
-//    for (const auto& entry : t_dir) // Burco los directorios dentro de "directory"
-//    {
-//        if (entry.is_directory()) // Si es un fichero tipo directorio
-//        {
-//            std::string path = entry.path().string(); // Me guardo su ruta en "path"
-//            std::string new_Path = parsePath(path);
-//            // Escribe en el archivo todas las rutas que encuentro
-//            t_output << "FileSystem=" << new_Path << '\n';
-//            std::filesystem::directory_iterator d(new_Path);
-//            findDir(d, t_output);
-//        }
-//        else
-//        {
-//            std::string x = entry.path().string();
-//            std::string aux = x.substr(x.size() - 3, 1) + x.substr(x.size() - 2, 1) + x.substr(x.size() - 1, 1);
-//            if (aux == "zip")
-//            {
-//                x = parsePath(x);
-//                t_output << "Zip=" << x << '\n';
-//            }
-//        }
-//    }
-//}
+//     for (const auto& entry : t_dir) // Burco los directorios dentro de "directory"
+//     {
+//         if (entry.is_directory()) // Si es un fichero tipo directorio
+//         {
+//             std::string path = entry.path().string(); // Me guardo su ruta en "path"
+//             std::string new_Path = parsePath(path);
+//             // Escribe en el archivo todas las rutas que encuentro
+//             t_output << "FileSystem=" << new_Path << '\n';
+//             std::filesystem::directory_iterator d(new_Path);
+//             findDir(d, t_output);
+//         }
+//         else
+//         {
+//             std::string x = entry.path().string();
+//             std::string aux = x.substr(x.size() - 3, 1) + x.substr(x.size() - 2, 1) + x.substr(x.size() - 1, 1);
+//             if (aux == "zip")
+//             {
+//                 x = parsePath(x);
+//                 t_output << "Zip=" << x << '\n';
+//             }
+//         }
+//     }
+// }
 //
-//void loadDirectories()
+// void loadDirectories()
 //{
-//    std::string directory = "./Assets";    // Directorio donde estan todos los recursos que buscar
-//    std::ifstream infile("resources.cfg"); // Archivo de input
-//    std::string line;                      // Linea donde se guarda cada linea leida
-//    std::vector<std::string> text;         // Vector donde me guardo todo el texto leido. Cada componente del vector es una linea
+//     std::string directory = "./Assets";    // Directorio donde estan todos los recursos que buscar
+//     std::ifstream infile("resources.cfg"); // Archivo de input
+//     std::string line;                      // Linea donde se guarda cada linea leida
+//     std::vector<std::string> text;         // Vector donde me guardo todo el texto leido. Cada componente del vector es una linea
 //
-//    // si no puede abrir resources.cfg ERROR
-//    try
-//    {
-//        if (!infile)
-//        {
-//            throw std::ifstream::failure("resources.cfg dont found");
-//        }
-//    }
-//    catch (std::ifstream::failure& excepcion)
-//    {
-//        std::cerr << "Error: " << excepcion.what() << '\n';
-//        exit(1);
-//    }
+//     // si no puede abrir resources.cfg ERROR
+//     try
+//     {
+//         if (!infile)
+//         {
+//             throw std::ifstream::failure("resources.cfg dont found");
+//         }
+//     }
+//     catch (std::ifstream::failure& excepcion)
+//     {
+//         std::cerr << "Error: " << excepcion.what() << '\n';
+//         exit(1);
+//     }
 //
-//    while (line != "FileSystem=./Assets") // Leo hasta "FileSystem=./Assets" que es lo que no quiero sobreescribir
-//    {
-//        getline(infile, line);
-//        text.push_back(line);
-//    }
-//    infile.close(); // Cierro el archivo
+//     while (line != "FileSystem=./Assets") // Leo hasta "FileSystem=./Assets" que es lo que no quiero sobreescribir
+//     {
+//         getline(infile, line);
+//         text.push_back(line);
+//     }
+//     infile.close(); // Cierro el archivo
 //
-//    std::ofstream output("resources.cfg" /*, std::ios::app | std::ios::ate*/); // Archivo para output
-//for (int i = 0; i < text.size(); i++)                                          // Escribo en el archivo todas las lineas anteriores que quiero conservar
+//     std::ofstream output("resources.cfg" /*, std::ios::app | std::ios::ate*/); // Archivo para output
+// for (int i = 0; i < text.size(); i++)                                          // Escribo en el archivo todas las lineas anteriores que quiero conservar
 //{
-//    line = text[i];
-//    output << line << '\n';
-//}
-//std::filesystem::directory_iterator dir;
+//     line = text[i];
+//     output << line << '\n';
+// }
+// std::filesystem::directory_iterator dir;
 //
 //// abrimos el directorio y si lanza excepcion la capturamos
-//try
+// try
 //{
-//    dir = std::filesystem::directory_iterator(directory); // si no existe la carpeta de Assets lanzamos excepcion
-//}
-//catch (std::filesystem::filesystem_error& e)
+//     dir = std::filesystem::directory_iterator(directory); // si no existe la carpeta de Assets lanzamos excepcion
+// }
+// catch (std::filesystem::filesystem_error& e)
 //{
-//    std::cerr << "ERROR: " << e.what() << "\n";
-//    exit(1);
-//}
+//     std::cerr << "ERROR: " << e.what() << "\n";
+//     exit(1);
+// }
 //// Metod recursivo para buscar todos los directorios
-//findDir(dir, output);
+// findDir(dir, output);
 //
-//output.close(); // Cierro el archivo ���IMPORTANTE PARA QUE SE HAGA BIEN LA LECTURA Y ESCRITURA!!!
-//}
+// output.close(); // Cierro el archivo ���IMPORTANTE PARA QUE SE HAGA BIEN LA LECTURA Y ESCRITURA!!!
+// }
 //
-//int main()
+// int main()
 //{
-//    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-//    loadDirectories();
-//    MiFlamingo* motorFlamingo = new MiFlamingo();
-//    if (motorFlamingo->setup())
-//    {
-//        motorFlamingo->loop();
-//        motorFlamingo->exit();
-//    }
-//    else
-//    {
-//        delete motorFlamingo;
-//        return -1;
-//    }
+//     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//     loadDirectories();
+//     MiFlamingo* motorFlamingo = new MiFlamingo();
+//     if (motorFlamingo->setup())
+//     {
+//         motorFlamingo->loop();
+//         motorFlamingo->exit();
+//     }
+//     else
+//     {
+//         delete motorFlamingo;
+//         return -1;
+//     }
 //
-//    delete motorFlamingo;
+//     delete motorFlamingo;
 //
-//    return 0;
-//}
+//     return 0;
+// }
