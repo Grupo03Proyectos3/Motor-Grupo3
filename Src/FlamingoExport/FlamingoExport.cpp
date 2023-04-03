83e3b87f692466f1c80e0e6c5045f967ed7cfe89
elisatodd <elitodd@ucm.es>
2023-04-03 18:02:09 +0200

    Probando exportaciones

---
+++ Src/FlamingoExport/FlamingoExport.cpp
@@ -0,0 +1,40 @@
+// FlamingoExport.cpp : Define las funciones exportadas del archivo DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "FlamingoExport.h"
+#include <iostream>
+
+//// Ejemplo de variable exportada
+// FLAMINGOEXPORT_API int nFlamingoExport=0;
+//
+//// Ejemplo de funciÃ³n exportada.
+// FLAMINGOEXPORT_API int fnFlamingoExport(void)
+//{
+//     return 0;
+// }
+
+CFlamingoExport* CFlamingoExport::_instance = nullptr;
+
+bool CFlamingoExport::Init()
+{
+    _instance = new CFlamingoExport();
+    return true;
+}
+
+CFlamingoExport* CFlamingoExport::Instance()
+{
+    return _instance;
+}
+
+// Constructor de clase exportada.
+CFlamingoExport::CFlamingoExport()
+{
+    return;
+}
+
+void CFlamingoExport::DoSomething()
+{
+    std::cout << "CFlamingoExport::DoSomething() : " << _instance << "\n";
+}
