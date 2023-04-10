c47308ed7782cdc058df33411d062bd4f5051fd1
RaulSaavedraRiera <rsaave02@ucm.es>
2023-04-10 12:58:19 +0200

    Descomentado metodo que si tiene que estar activo

--- Src/Lua/LuaSystem.h
+++ Src/Lua/LuaSystem.h
@@ -53,19 +53,19 @@ namespace Flamingo
 
         void readScript(const std::string& t_name);
         void callLuaFunction(std::string t_name);
-        // template <class... Args>
-        // void callLuaFunction(std::string name, Args&&...args);
-      /*  void addNumToLua(float var, std::string name);
-        void addIntToLua(int var, std::string name);
-        void addTransSpaceToLua(Camera::transformSpace t_trs, std::string t_var_name);
-        void addPolygonModeToLua(Camera::polygonMode t_pm, std::string t_var_name);
-        void addLightTypeToLua(Light::lightType t_type, std::string t_var_name);
-        void addBooleanToLua(bool var, std::string t_name);
-        void addColorToLua(SColor t_color, std::string t_var_name);
-        void addVector3ToLua(SVector3 t_vec, std::string t_var_name);
-        void addCameraToLua(Camera* t_cam, std::string t_var_name);
-        void addLightToLua(Light* t_light, std::string t_var_name);
-        void addMeshRendererToLua(MeshRenderer* t_mr, std::string t_var_name);*/
+        //// template <class... Args>
+        //// void callLuaFunction(std::string name, Args&&...args);
+        //void addNumToLua(float var, std::string name);
+        //void addIntToLua(int var, std::string name);
+        //void addTransSpaceToLua(Camera::transformSpace t_trs, std::string t_var_name);
+        //void addPolygonModeToLua(Camera::polygonMode t_pm, std::string t_var_name);
+        //void addLightTypeToLua(Light::lightType t_type, std::string t_var_name);
+        //void addBooleanToLua(bool var, std::string t_name);
+        //void addColorToLua(SColor t_color, std::string t_var_name);
+        //void addVector3ToLua(SVector3 t_vec, std::string t_var_name);
+        //void addCameraToLua(Camera* t_cam, std::string t_var_name);
+        //void addLightToLua(Light* t_light, std::string t_var_name);
+        //void addMeshRendererToLua(MeshRenderer* t_mr, std::string t_var_name);
         
         template <typename T>
         void addVarToLua(T t_var, std::string t_name)
