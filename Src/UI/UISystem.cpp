dd526d530052aee2d6ea27b82626517e7eaccd9e
elisatodd <elitodd@ucm.es>
2023-04-11 10:15:07 +0200

    Cambio en los mensajes de UI
    Co-Authored-By: Miguel González Pérez <82326243+miggon23@users.noreply.github.com>

--- Src/UI/UISystem.cpp
+++ Src/UI/UISystem.cpp
@@ -1,55 +1,73 @@
 #include "UISystem.h"
+#include "UIElement.h"
 #include <CEGUI/Base.h>
-#include <CEGUI/Window.h>
-#include <iostream>
-#include <OgreRenderWindow.h>
 #include <CEGUI/RendererModules/Ogre/ResourceProvider.h>
 #include <CEGUI/System.h>
-#include "UIElement.h"
+#include <CEGUI/Window.h>
+#include <OgreRenderWindow.h>
+#include <iostream>
 
-namespace Flamingo{
-    UISystem::UISystem(){
+namespace Flamingo
+{
+    UISystem::UISystem()
+    {
     }
 
-    UISystem::~UISystem(){
-        winMngr->destroyAllWindows();  
+    UISystem::~UISystem()
+    {
+        winMngr->destroyAllWindows();
     }
 
-    void UISystem::recieve(const Message& m){
+    void UISystem::recieve(const Message& m)
+    {
+        if (!(m.id == MSG_WINDOW_RESIZED || m.id == MSG_TRANSFORM_MOVE || m.id == MSG_TRANSFORM_ROTATE || m.id == MSG_TRANSFORM_SCALING))
+        {
+            return;
+        }
+
         UIElement* element = nullptr;
         Transform* transform = nullptr;
-        if (m.entity_affected != nullptr){
-            element = m_mngr->getComponent<UIElement>(m.entity_affected);
-            transform = m_mngr->getComponent<Transform>(m.entity_affected);
-        }
+        element = m_mngr->getComponent<UIElement>(m.entity_affected);
+        transform = m_mngr->getComponent<Transform>(m.entity_affected);
         if (element == nullptr)
+        {
             return;
-        switch (m.id)
+        }
+
+        switch (m.id) 
         {
             case MSG_WINDOW_RESIZED:
                 chageScreenSize(m_mngr->getSystem<RenderSystem>()->getWindow()->getRenderWindow()->getWidth(), m_mngr->getSystem<RenderSystem>()->getWindow()->getRenderWindow()->getHeight());
             case MSG_TRANSFORM_MOVE:
-                element->setPosition(transform->getPosition());              
+            {
+                element->setPosition(transform->getPosition());
                 break;
+            }
             case MSG_TRANSFORM_ROTATE:
+            {
                 element->setRotation(transform->getRotation());
                 break;
+            }
             case MSG_TRANSFORM_SCALING:
+            {
                 element->setPosition(transform->getScale());
                 break;
+            }
             default:
                 break;
         }
     }
 
-    void UISystem::initSystem(){
-        initUIResources(); 
+    void UISystem::initSystem()
+    {
+        initUIResources();
     }
 
-    void UISystem::update(float t_delta_time){
+    void UISystem::update(float t_delta_time)
+    {
         renderer->beginRendering();
         guiContext->draw();
-        renderer->endRendering();     
+        renderer->endRendering();
     }
 
     void UISystem::initContext()
@@ -64,11 +82,11 @@ namespace Flamingo{
 
         initRoot();
         pruebas();
-
     }
 
-    void UISystem::initRoot(){
-        eraseMainRoot();     
+    void UISystem::initRoot()
+    {
+        eraseMainRoot();
         root = winMngr->createWindow("DefaultWindow", "Root");
 
         root->setUsingAutoRenderingSurface(true);
@@ -78,7 +96,8 @@ namespace Flamingo{
         root->activate();
     }
 
-    void UISystem::initUIResources(){
+    void UISystem::initUIResources()
+    {
         CEGUI::ImageManager::setImagesetDefaultResourceGroup("General");
         CEGUI::Font::setDefaultResourceGroup("General");
         CEGUI::Scheme::setDefaultResourceGroup("General");
@@ -86,40 +105,46 @@ namespace Flamingo{
         CEGUI::WindowManager::setDefaultResourceGroup("General");
     }
 
-    void UISystem::eraseContext(){
+    void UISystem::eraseContext()
+    {
         renderer->setUsingShaders(false);
         eraseMainRoot();
         CEGUI::System::getSingleton().destroyGUIContext(*guiContext);
-        renderer->destroySystem();      
+        renderer->destroySystem();
     }
 
-    void UISystem::eraseMainRoot(){
+    void UISystem::eraseMainRoot()
+    {
         if (root != nullptr)
             root->destroy();
 
         root = nullptr;
     }
 
-    void UISystem::loadScheme(const std::string& schemeFile){
-       auto x= CEGUI::SchemeManager::getSingletonPtr();
+    void UISystem::loadScheme(const std::string& schemeFile)
+    {
+        auto x = CEGUI::SchemeManager::getSingletonPtr();
         x->createFromFile(schemeFile);
     }
 
-    void UISystem::setFont(const std::string& fontFile){
+    void UISystem::setFont(const std::string& fontFile)
+    {
         CEGUI::FontManager::getSingleton().createFromFile(fontFile + ".font");
         guiContext->setDefaultFont(fontFile);
     }
 
-    CEGUI::Window* UISystem::createWidget(const std::string& type, const std::string& name){
+    CEGUI::Window* UISystem::createWidget(const std::string& type, const std::string& name)
+    {
         CEGUI::Window* newWindow = winMngr->createWindow(type, name);
         root->addChild(newWindow);
         newWindow->activate();
-        newWindow->setPosition(CEGUI::UVector2(CEGUI::UDim(0.5,0.5), CEGUI::UDim(0.5, 0.5)));
+        newWindow->setPosition(CEGUI::UVector2(CEGUI::UDim(0.5, 0.5), CEGUI::UDim(0.5, 0.5)));
         newWindow->setSize(CEGUI::USize(CEGUI::UDim(0.3, 0.3), CEGUI::UDim(0.2, 0.5)));
         return newWindow;
     }
 
-    CEGUI::Window* UISystem::createEmptyWindow(const std::string& name) {
+    CEGUI::Window* UISystem::createEmptyWindow(const std::string& name)
+    {
         CEGUI::Window* newWindow = winMngr->createWindow("DefaultWindow", name);
         root->addChild(newWindow);
         newWindow->activate();
@@ -128,13 +153,15 @@ namespace Flamingo{
         return newWindow;
     }
 
-    void UISystem::chageScreenSize(int width, int height){
+    void UISystem::chageScreenSize(int width, int height)
+    {
         renderer->setDisplaySize(CEGUI::Size<float>(width, height));
-        CEGUI::System::getSingleton().notifyDisplaySizeChanged(CEGUI::Size<float>(width, height));        
+        CEGUI::System::getSingleton().notifyDisplaySizeChanged(CEGUI::Size<float>(width, height));
     }
 
-    void UISystem::pruebas(){
+    void UISystem::pruebas()
+    {
         loadScheme("TaharezLook.scheme");
-        setFont("DejaVuSans-10");      
+        setFont("DejaVuSans-10");
     }
 } // namespace Flamingo
