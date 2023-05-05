cc1a872aa9c11a92dd5b0f5a42d15d216384531e
admont02 <admont02@ucm.es>
2023-05-05 21:47:45 +0200

    Update ManagerFunctions.h

--- Src/ECS/ManagerFunctions.h
+++ Src/ECS/ManagerFunctions.h
@@ -4,18 +4,22 @@
 #define __MANAGER_FUNCTIONS_H__
 
 #include "Component.h"
+#include "FlamingoBase/SceneManager.h"
 #include "FlamingoBase/Transform.h"
 #include "FlamingoExport/FlamingoCore.h"
-#include "FlamingoBase/SceneManager.h"
-#include "Scripting/ScriptManager.h"
 #include "Manager.h"
+#include "Scripting/ScriptManager.h"
 #include "ecs.h"
 
 namespace Flamingo
 {
 
-    /*Añade al gameObejct dado y devuelve el componente, solicitandolo al manager directamente o pasando por el ScriptManager según
-     * si pertenece al motor o ha sido creado por el ususario del motor */
+    /**
+     * @brief Añade al GameObject dado y devuelve el componente, solicitandolo al Manager directamente o pasando por el ScriptManager, según
+     * si pertenece al motor o ha sido creado por el usuario del motor
+     *
+     * @param[in] gO GameObject
+     */
     template <typename T>
     __declspec(dllexport) inline T* addComponent(GameObject* gO)
     {
@@ -25,7 +29,7 @@ namespace Flamingo
         if (!std::is_base_of_v<BehaviourScript, T>)
         {
             auto c = Manager::instance()->addComponent<T>(gO);
-            //dynamic_cast<Component*>(c)->initComponent();
+
             return c;
         }
         else
@@ -38,8 +42,12 @@ namespace Flamingo
         }
     }
 
-    /*Elimina componente del GameObject si posee, solicitandolo al manager directamente o pasando por el ScriptManager según
-     * si pertenece al motor o ha sido creado por el ususario del motor */
+    /**
+     * @brief Elimina componente del GameObject si posee, solicitandolo al manager directamente o pasando por el ScriptManager, según
+     * si pertenece al motor o ha sido creado por el usuario del motor
+     *
+     * @param[in] gO GameObject
+     */
     template <typename T>
     __declspec(dllexport) inline void removeComponent(GameObject* gO)
     {
@@ -52,8 +60,12 @@ namespace Flamingo
             ScriptManager::instance()->removeScript(typeid(T).name(), gO);
     }
 
-    /*Devuelve el componente, solicitandolo al manager directamente o pasando por el ScriptManager según
-     * si pertenece al motor o ha sido creado por el ususario del motor */
+    /**
+     * @brief Devuelve componente del GameObject, solicitandolo al manager directamente o pasando por el ScriptManager, según
+     * si pertenece al motor o ha sido creado por el usuario del motor
+     *
+     * @param[in] gO GameObject
+     */
     template <typename T>
     __declspec(dllexport) inline T* getComponent(GameObject* gO)
     {
@@ -66,8 +78,12 @@ namespace Flamingo
             return dynamic_cast<T*>(ScriptManager::instance()->getScript(typeid(T).name(), gO));
     }
 
-    /*Comprueba si el gameObject dado contiene el componente, solicitandolo al manager directamente o pasando por el ScriptManager según
-     * si pertenece al motor o ha sido creado por el ususario del motor */
+    /**
+     * @brief Comprueba si el GameObject dado contiene el componente,, solicitandolo al manager directamente o pasando por el ScriptManager, según
+     * si pertenece al motor o ha sido creado por el usuario del motor
+     *
+     * @param[in] gO GameObject
+     */
     template <typename T>
     __declspec(dllexport) inline bool hasComponent(GameObject* gO)
     {
@@ -80,13 +96,19 @@ namespace Flamingo
             return ScriptManager::instance()->hasScript(typeid(T).name(), gO);
     }
 
-    /*Solicita al manager que se cree un gameObject y se le añada a los grupos proporcionados; asi como se le añada un componente
-    transform por defecto*/
+    /**
+     * @brief Solicita al Manager que se cree un GameObject y se le añada a los grupos proporcionados; asi como se le añada un componente
+     * Transform por defecto
+     *
+     * @param[in] std::string t_n nombre para el GameObject
+     * @param[in] std::vector<groupId_type> T_vect_gId grupos a los que añadir el GameObject
+     *   @return GameObject*
+     */
     __declspec(dllexport) inline GameObject* createGameObject(std::string t_n, std::vector<groupId_type> t_vect_gId = {})
     {
         auto gO = Manager::instance()->addGameObject(t_vect_gId);
         gO->setName(t_n);
-        auto t =addComponent<Transform>(gO);
+        auto t = addComponent<Transform>(gO);
         t->initValues();
         t->initComponent();
         FlamingoCore::instance()->getSceneManager()->getSceneActive()->addObjects(gO);
