cb95a2c67c667403951331ee03faf7c7c5fc6213
admont02 <admont02@ucm.es>
2023-05-05 22:14:15 +0200

    Update Component.h

--- Src/ECS/Component.h
+++ Src/ECS/Component.h
@@ -12,30 +12,28 @@ namespace Flamingo
       public:
         Component();
 
-        // Destroys the component.
-        // Careful! ent_ and mngr_ should not be destroyed
-        //
+        
         virtual ~Component() {};
-        // This method is used to set the context, which is a
-        // reference to the entity in which the component is
-        // installed and a reference to the manager. It will
-        // be called by Entity when adding a component.
-        //
+        
+        /**
+         * @brief Método que otorga contexto a la entidad
+         *
+         * @param[in] t_ent GameObject entidad
+         * @param[in] t_mngr Manager 
+         */
         void setContext(GameObject* t_ent, Manager* t_mngr);
-        // We assume that initComponent will be called when adding a
-        // component to an entity, immediately after setContext.
-        //
+        /**
+         * @brief Método que se llama al añadir un Compnente a un GameObject para inicializarlo
+         *
+         */
         virtual void initComponent(){};
 
         GameObject* gameObject();
 
       protected:
-        // as mentions above, when using systems these fields are not
-        // really needed, but we keep them for now from the same reason that
-        // we keep update/render
 
-        GameObject* m_ent; // a reference to the entity, should not be deleted on destruction
-        Manager* m_mngr;   //  a pointer to the manager, should not be deleted on destruction
+        GameObject* m_ent; // referencia a la entidad
+        Manager* m_mngr;   // puntero al manager
     };
 } // namespace Flamingo
 
