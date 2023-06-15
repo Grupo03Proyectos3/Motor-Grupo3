09065e3449d34752aa51a6c87108144b3786b81d
AntonioPove <79278677+AntonioPove@users.noreply.github.com>
2023-06-15 18:35:38 +0200

    Añadida parte de la documentacion de scenemanager

--- Src/FlamingoBase/SceneManager.h
+++ Src/FlamingoBase/SceneManager.h
@@ -31,26 +31,94 @@ namespace Flamingo
         ~SceneManager();
         void initManager(std::string t_Name, Manager* m_mng);
 
+        /**
+         * @brief Crea una nueva escena y la activa/desactiva
+         *
+         * @param[in] t_SceneName nombre de la nueva escena
+         * @param[in] setActive si la escena estará activa o no
+         *
+         * @return Scene*
+         */
         Scene* createScene(std::string t_SceneName, bool setActive = false);
+
+         /**
+         * @brief Borra una escena
+         *
+         * @param[in] t_SceneName nombre de la escena a borrar
+         *
+         * @return void
+         */
         void delScene(std::string t_SceneName);
 
+        /**
+         * @brief Pone una escena como activa, desactivando antes la escena activa
+         * Además se guarda referencias a esta nueva escena
+         *
+         * @param[in] t_SceneName nombre de la nueva escena activa
+         *
+         * @return void
+         */
         void setSceneActive(std::string t_SceneName);
+
+        /**
+         * @brief Devuelve cual es la escena que esta actualmente activa
+         *
+         *
+         * @return Scene*
+         */
         Scene* getSceneActive();
+
+        /**
+         * @brief Obtiene la referencia a una escena en concreto
+         *
+         * @param[in] t_SceneName nombre de la escena
+         * 
+         * @return Scene*
+         */
         Scene* getScene(std::string t_scene_name);
 
+         /**
+         * @brief Añade una escena al unordered_map m_scenes donde se guardan todas
+         * las referencias a estas. Si ya existia una escena con ese nombre
+         * salta un excepcion
+         *
+         * @param[in] t_Scene escena a añadir
+         *
+         * @return void
+         */
         void addScene(Scene* t_Scene);
 
+          /**
+         * @brief Modifica el valor de m_reloadScene para permitir que se 
+         * vuelva a recargar la escena actual
+         *
+         * 
+         * @return void
+         */
         void reloadScenePetition();
 
+         /**
+         * @brief Modifica el valor de m_reloadScene para permitir que se
+         * vuelva a recargar la escena
+         *
+         * @param[in] t_sceneName escena que se quiere recargar
+         *
+         * @return void
+         */
         void startScene(std::string t_sceneName);
 
-         void reloadScene();
+         /**
+         * @brief Si m_reloadScene es verdadero se borra la informacion de la escena a recargar una escena
+         * para volverla a crear todos los elementos de esta, poniedola como activa
+         *
+         * @return void
+         */
+        void reloadScene();
 
-         Scene* getSceneToAttach();
-         void setSceneToAttach(Scene* t_sceneToAttach);
+        Scene* getSceneToAttach();
+        void setSceneToAttach(Scene* t_sceneToAttach);
 
       private:
-
         Manager* m_mngr;
 
         std::unordered_map<std::string, Scene*> m_scenes;
