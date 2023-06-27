6076f01e838705d7cf0b0a8d3b832ea121e261bc
ssimoanto <simoanto@ucm.es>
2023-06-28 00:46:18 +0200

    Update ParticleSystem.h

--- Src/Render/ParticleSystem.h
+++ Src/Render/ParticleSystem.h
@@ -13,33 +13,39 @@ namespace Ogre
     class ParticleSystem;
     class SceneManager;
     class SceneNode;
-}
+} // namespace Ogre
 
 #include <string>
 namespace Flamingo
 {
+    /**
+     * @brief Componente del sistema de partículas.
+     * Para utilizar sistemas existentes, debes leerlos a través del archivo LUA,
+     * proporcionando el nombre que deseas asignarle y la plantilla que deseas usar como parámetros.
+     * Para crear una nueva plantilla, es necesario definirlo en el archivo SistemasParticulas.material.
+     * Los nombres asignados a los sistemas de partículas deben ser distintos
+     **/
     struct FLAMINGOEXPORT_API ParticleSystem : public Component
     {
       public:
         __SYSTEM_ID_DECL__(_cpm_PARTICLE_SYSTEM)
 
         ParticleSystem();
-       
+
         ~ParticleSystem();
         void initComponent() override;
         void initValues(const std::string& t_name, const std::string& t_template, bool t_static);
+
         void emit(bool t_state);
-        void updatePosition() ;
+        void updatePosition();
 
       private:
-        Ogre::ParticleSystem* m_particle_system = nullptr;
+        Ogre::ParticleSystem* m_particle_system;
         Ogre::SceneManager* m_scene_mngr;
         Ogre::SceneNode* m_node;
-       
 
         std::string m_name;
         bool m_static;
-       
 
         bool m_active;
     };
